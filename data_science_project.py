9a2de9171cbdd94b2ecd8394bf16d8995074d0a8
rajeswaritatikonda <rajeswaritatikonda27@gmail.com>
2025-08-01 15:06:58 +0530

    Update data_science_project.py

--- data_science_project.py
+++ data_science_project.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Title of the application
-st.title('🍕🍕Food Delivery Apps Data Analysis🍔🍔')
+st.title('🍕Food Delivery Apps Data Analysis🍔')
 
 # File upload
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
